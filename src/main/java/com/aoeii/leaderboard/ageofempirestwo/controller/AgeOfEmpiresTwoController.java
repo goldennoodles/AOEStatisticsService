d89688349080d54488603907535a021dbc83b65d
Rus Kuzmin <ruskuzmin@icloud.com>
2020-06-03 17:30:24 +0100

    Minor Clean up!

--- src/main/java/com/aoeii/leaderboard/ageofempirestwo/controller/AgeOfEmpiresTwoController.java
+++ src/main/java/com/aoeii/leaderboard/ageofempirestwo/controller/AgeOfEmpiresTwoController.java
@@ -1,31 +1,22 @@
 package com.aoeii.leaderboard.ageofempirestwo.controller;
 
-import com.aoeii.leaderboard.ageofempirestwo.model.MatchHistoryModel;
-import com.aoeii.leaderboard.ageofempirestwo.model.PlayerHistoryModel;
 import com.aoeii.leaderboard.ageofempirestwo.properties.ApplicationProperties;
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jayway.jsonpath.Configuration;
 import com.jayway.jsonpath.JsonPath;
-import org.apache.coyote.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.View;
-
-import javax.lang.model.type.ArrayType;
-import java.lang.management.PlatformLoggingMXBean;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
+
 
 @Component
 @RestController
@@ -55,7 +46,8 @@ public class AgeOfEmpiresTwoController {
                     + PlayerStats +
                     "//======= Match Stats ==========" + "\n"
                     + MatchStats + "\n";
-        } catch (Exception ex) {
+
+        } catch (HttpClientErrorException ex) {
             return ex.getMessage();
         }
     }
--- src/main/java/com/aoeii/leaderboard/ageofempirestwo/properties/ApplicationProperties.java
+++ src/main/java/com/aoeii/leaderboard/ageofempirestwo/properties/ApplicationProperties.java
@@ -12,13 +12,13 @@ public class ApplicationProperties {
 
     public final String AOE_API_LOCATION = "https://aoe2.net/api/strings?game=aoe2de&language=en";
     public final String AOE_LEADER_BOARD_LOCATION = "https://aoe2.net/api/leaderboard?game=aoe2de&leaderboard_id=3&start=1&count=1";
-    public String AOE_USER = "76561198048932097";
+    public String AOE_USER = "76561198048932097"; // Make Dynamic (Current : Me)
     public final String PLAYER_MATCH_HISTORY = "https://aoe2.net/api/player/matches?game=aoe2de&steam_id=" + AOE_USER + "&count=5";
 
     /*
-    Jahan = 76561197989421548
-    Hayden = 76561198080895557
-    Ralph = 76561198236126814
-    Geroge = 76561198835871299
+        Jahan = 76561197989421548
+        Hayden = 76561198080895557
+        Ralph = 76561198236126814
+        George = 76561198835871299
      */
 }
